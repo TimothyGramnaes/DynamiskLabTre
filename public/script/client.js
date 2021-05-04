56db3528990268e05650fca24653c56635cec889
Olof-Wallgren <naturel.olof@hotmail.com>
2021-05-04 15:22:26 +0200

    some toublewith rendering

--- public/script/client.js
+++ public/script/client.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", () => {
-  console.log("event");
-});
+// window.addEventListener("load", () => {
+//     console.log("event");
+// });
 
 const shatForm = document.getElementById("message-form");
 
@@ -8,29 +8,30 @@ const socket = io();
 
 // Recieved messages from the server for render
 socket.on("message", (message) => {
-  console.log(message);
-  renderMessage(message);
+    console.log(message);
+    renderMessage(message);
 });
 
 shatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const shatMsg = e.target.elements.message.value;
+    const shatMsg = e.target.elements.message.value;
 
-  // Send message to be read by the server
-  socket.emit("shatMessage", shatMsg);
-  console.log(shatMsg);
+    // Send message to be read by the server
+    socket.emit("shatMessage", shatMsg);
+    console.log(shatMsg);
 });
 
 function renderMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("user-message");
-  div.innerHTML = `<div class="message-header">
+    console.log('render message')
+    const div = document.createElement("div");
+    div.classList.add("message-body");
+    div.innerHTML = `<div class="message-header">
     <h5>Bosse</h5>
     <p class="time">13:46</p>
-</div>
+    </div>
     <p class="message-body">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laborum quas similique.</div>
-    </p>             
-`;
+    ${message}hejhejhejhejhe
+    </p>`;
+    document.querySelector('.chat').appendChild(div);
 }
