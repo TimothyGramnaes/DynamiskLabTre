1fc0b5959fd60086a0ee0782db2500b64dd1bda4
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-05-04 18:52:07 +0200

    still stable

--- public/script/client.js
+++ public/script/client.js
@@ -3,35 +3,40 @@
 // });
 
 const shatForm = document.getElementById("message-form");
+const shatContainer = document.querySelector(".chat");
 
 const socket = io();
 
 // Recieved messages from the server for render
-socket.on("message", (message) => {
-    console.log(message);
-    renderMessage(message);
+socket.on("shatMessage", (message) => {
+  console.log(message);
+  renderMessage(message);
+
+  // scroll down upon writing messages
+  window.scrollTo(0, document.body.scrollHeight);
 });
 
 shatForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const shatMsg = e.target.elements.message.value;
+  const shatMsg = e.target.elements.message.value;
 
-    // Send message to be read by the server
-    socket.emit("message", shatMsg);
-    console.log(shatMsg);
+  // Send message to be read by the server
+  socket.emit("shatMessage", shatMsg);
+  console.log(shatMsg);
 });
 
+// Rendering messages from the server as content on the chat
 function renderMessage(message) {
-    console.log('render message')
-    const div = document.createElement("div");
-    div.classList.add("user-message");
-    div.innerHTML = `<div class="message-header">
+  console.log("render message");
+  const div = document.createElement("div");
+  div.classList.add("user-message");
+  div.innerHTML = `<div class="message-header">
     <h5>Bosse</h5>
     <p class="time">13:46</p>
     </div>
     <p class="message-body">
     ${message}
     </p>`;
-    document.querySelector('.chat').appendChild(div);
+  document.querySelector(".chat").appendChild(div);
 }
