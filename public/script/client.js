2443f0f98aa24cbfebf03e4a4d1b9f5ccd9ec51c
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-05-04 18:24:28 +0200

    hejsan

--- public/script/client.js
+++ public/script/client.js
@@ -8,30 +8,34 @@ const socket = io();
 
 // Recieved messages from the server for render
 socket.on("message", (message) => {
-    console.log(message);
-    renderMessage(message);
+  console.log(message);
+  // renderMessage(message);
+});
+
+socket.on("shatMessage", (shatMsg) => {
+  renderMessage(shatMsg);
 });
 
 shatForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const shatMsg = e.target.elements.message.value;
+  const shatMsg = e.target.elements.message.value;
 
-    // Send message to be read by the server
-    socket.emit("shatMessage", shatMsg);
-    console.log(shatMsg);
+  // Send message to be read by the server
+  socket.emit("shatMessage", shatMsg);
+  console.log(shatMsg);
 });
 
 function renderMessage(message) {
-    console.log('render message')
-    const div = document.createElement("div");
-    div.classList.add("message-body");
-    div.innerHTML = `<div class="message-header">
+  console.log("render message");
+  const div = document.createElement("div");
+  div.classList.add("user-message");
+  div.innerHTML = `<div class="message-header">
     <h5>Bosse</h5>
     <p class="time">13:46</p>
     </div>
     <p class="message-body">
-    ${message}hejhejhejhejhe
+    ${message}
     </p>`;
-    document.querySelector('.chat').appendChild(div);
+  document.querySelector(".chat").appendChild(div);
 }
