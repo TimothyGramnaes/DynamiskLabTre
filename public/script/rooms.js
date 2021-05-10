9538648deec4ca2995d2b548843c249a9de441cf
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-10 21:13:25 +0200

    working duplicate check and create room functionality

--- public/script/rooms.js
+++ public/script/rooms.js
@@ -2,74 +2,42 @@ const createRoomBtn = document.getElementById("create-room-btn");
 const newRoom = document.getElementById("room-name");
 const option = document.getElementsByTagName('option')
 const statusText = document.getElementById('status-text')
-let optionValue = '';
 let optionValuesArray = []
-
-// const socket = io();
-
-// const activeRooms = [];
+let createRoom = Boolean;
 
 
 createRoomBtn.addEventListener("click", () => {
 
-	// loopa igenom alla option's "value" som sedan ska jämföras med det nya 'newRoom'
+	// Create an array of the option elements "value" attributes
 	optionValuesArray = Array.from(roomDropdown.options)
 	
+	// Loop through array to check for
 	optionValuesArray.every(function (optionElement) {
-		optionValue = optionElement.value
-	
+		let optionValue = optionElement.value
 		
 		if (optionValue === newRoom.value) {
 			console.log('The room name', optionValue, 'already exists')
 			statusText.innerText = `A room called ${optionValue} already exists`
 			statusText.style.color = 'red'
+			createRoom = false
 			return false
 		} else {
 			console.log('Option value:', optionValue)
 			statusText.innerText = `The room ${newRoom.value} was added to list`
 			statusText.style.color = 'green'
+			createRoom = true
 			return true
 		}
 	})
 	
-	
-	
-	// ========== ORIGINALKOD NEDANFÖR =========================================== //
-	
-	// const newRoomName = newRoom.value;
-	
-	// const optionElement = document.createElement("option");
-	// roomDropdown.appendChild(optionElement);
-	// optionElement.setAttribute("value", newRoomName);
-	// optionElement.innerText = newRoomName;
-	
-	// console.log("New room created:", newRoomName);
-	
-	// ========== ORIGINALKOD OVANFÖR ============================================ //
+	if (createRoom) {
+		const newRoomName = newRoom.value;
 
+		const optionElement = document.createElement("option");
+		roomDropdown.appendChild(optionElement);
+		optionElement.setAttribute("value", newRoomName);
+		optionElement.innerText = newRoomName;
 
-
-
-	// ============ Potentiallt onödig kod?
-  // activeRooms.push(newRoomName);
-  // //   showActiveRooms();
-
-  // socket.emit("activeRooms", activeRooms);
-  // console.log(activeRooms);
+		console.log("New room created:", newRoomName);
+	}
 });
-
-//socket.on("")
-
-// function showActiveRooms() {
-//   // const rooms =
-//   // console.log("funkar det?", activeRooms);
-// }
-
-// user joins shat
-// function joiningUser(id, username, room) {
-//   const user = { id, username, room };
-
-//   users.push(user);
-
-//   return user;
-// }
