2e3164c91c80f853c94b586fc90afba75425130d
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-08 11:40:10 +0200

    working

--- public/script/rooms.js
+++ public/script/rooms.js
@@ -1,9 +1,11 @@
 const createRoomBtn = document.getElementById("create-room-btn");
-const newRoom = document.getElementById("room-name");
+const newRoomNameInput = document.getElementById("room-name");
 const option = document.getElementsByTagName('option')
 const statusText = document.getElementById('status-text')
+const newRoomPassword = document.getElementById('roomPassword')
 let optionValuesArray = []
 let createRoom = Boolean;
+let isPrivate = Boolean;
 
 
 createRoomBtn.addEventListener("click", () => {
@@ -11,12 +13,18 @@ createRoomBtn.addEventListener("click", () => {
 	// Create an array of the option elements "value" attributes
 	optionValuesArray = Array.from(roomDropdown.options)
 
+	if (newRoomPassword.value.length != 0) {
+		isPrivate = true
+	} else {
+		isPrivate = false
+	}
+
 	// Loop through array to check for duplicates
 	optionValuesArray.every(function (optionElement) {
 		let optionValue = optionElement.value
 
 		// Check if <option value="___"> is same as newRoom.value
-		if (optionValue === newRoom.value) {
+		if (optionValue === newRoomNameInput.value) {
 			console.log('The room name', optionValue, 'already exists')
 			statusText.innerText = `A room called ${optionValue} already exists`
 			statusText.style.color = 'rgba(107, 0, 0, 1)'
@@ -24,7 +32,7 @@ createRoomBtn.addEventListener("click", () => {
 			return false // to break the loop
 		} else {
 			console.log('Option value:', optionValue)
-			statusText.innerText = `The room ${newRoom.value} was added to list`
+			statusText.innerText = `The room ${newRoomNameInput.value} was added to list`
 			statusText.style.color = 'rgba(26, 255, 121, 1)'
 			createRoom = true // to create room
 			return true
@@ -32,12 +40,18 @@ createRoomBtn.addEventListener("click", () => {
 	})
 
 	if (createRoom) {
-		const newRoomName = newRoom.value;
+		const newRoomName = newRoomNameInput.value;
 
 		const optionElement = document.createElement("option");
 		roomDropdown.appendChild(optionElement);
-		optionElement.setAttribute("value", newRoomName);
-		optionElement.innerText = newRoomName;
+
+		if (isPrivate) {
+			optionElement.setAttribute("value", newRoomName + ' - Privat');
+			optionElement.innerText = newRoomName + ' - Privat';
+		} else {
+			optionElement.setAttribute("value", newRoomName);
+			optionElement.innerText = newRoomName;
+		}
 
 		console.log("New room created:", newRoomName);
 	}
